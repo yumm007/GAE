cc3f14e9555c9bad9db396c0b451d0fb8262002a
unknown <yu@yu-hp-520.(none)>
2012-05-18 00:10:42 +0800

    添加文件

---
+++ pic/my-pic.py
@@ -0,0 +1,24 @@
+import cgi
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write("""
+          <html>
+            <body>
+		<img src="/pics/tt.jpg" />
+            </body>
+          </html>""")
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
